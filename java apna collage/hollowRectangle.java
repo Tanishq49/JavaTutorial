f2e33fe0d3ad05057c1b9aca6ff7c7c4a8c333c5
Tanishq <nottanishqbhandari@gmail.com>
2025-05-24 20:22:52 +0530

    Add files via upload

--- java apna collage/hollowRectangle.java
+++ java apna collage/hollowRectangle.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class hollowRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String green = "\u001B[32m";
+        String blue = "\u001B[34m";
+        String reset = "\u001B[0m";
         System.out.print("How many rows do you want?: ");
         int rows = sc.nextInt();
 
@@ -13,9 +15,9 @@ public class hollowRectangle {
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= cols; j++) {
                 if (i == 1 || i == rows) {
-                    System.out.print("- ");
+                    System.out.print(blue+"- "+reset);
                 } else if(j == 1 || j == cols){
-                    System.out.print("| ");
+                    System.out.print(green + "| "+ reset);
                 } 
                 else {
                     System.out.print("  ");
